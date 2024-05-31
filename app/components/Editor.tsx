dd5e613837c8096cdcb7bc02552ae1954af6fb01
levin-fankhauser <levin.fankhauser@gmx.ch>
2024-05-31 08:23:27 +0200

    Task-#26: Added color picker to editor component to log pixel data

--- app/components/Editor.tsx
+++ app/components/Editor.tsx
@@ -2,18 +2,42 @@
 
 import Canvas from "@/app/components/Canvas";
 import { PixelType } from "@/app/lib/definitions";
+import { hsvaToHex } from "@/node_modules/@uiw/color-convert";
+import Colorful from "@/node_modules/@uiw/react-color-colorful";
+import { useState } from "react";
 
 export default function Editor({ pixelData }: { pixelData: PixelType[] }) {
+  const [hsva, setHsva] = useState({ h: 0, s: 0, v: 68, a: 1 });
+
   const handlePixelUpdate = (pixel: PixelType) => {
-    console.log("Updated Pixel showed in Editor Component:", pixel);
+    console.log(
+      "Pixel Values:",
+      "x:",
+      pixel.x,
+      "y:",
+      pixel.y,
+      "color:",
+      hsvaToHex(hsva),
+    );
     // Connection to DB --> follows in Sprint 2
   };
 
   return (
-    <Canvas
-      pixelData={pixelData}
-      showOverlay={true}
-      onPixelClick={handlePixelUpdate}
-    />
+    <div className="flex flex-row">
+      <Canvas
+        pixelData={pixelData}
+        showOverlay={true}
+        onPixelClick={handlePixelUpdate}
+      />
+      <div className="ml-10 content-center">
+        <Colorful
+          color={hsva}
+          disableAlpha={true}
+          onChange={(color) => {
+            setHsva(color.hsva);
+          }}
+        />
+      </div>
+    </div>
   );
 }
