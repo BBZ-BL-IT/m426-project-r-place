f43f2cc835e546df517c60d8f6b16ba5422007f0
lorenzboss <lorenz.boss@bbzbl-it.ch>
2024-06-21 16:14:51 +0200

    TASK-#44: Update Header component.

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,44 +1,18 @@
 import NextLogo from "./NextLogo";
 import SupabaseLogo from "./SupabaseLogo";
+import NavButton from "@/app/components/NavButton";
+import AuthButton from "@/app/components/AuthButton";
+import { NavButtonProps } from "@/app/lib/definitions";
 
-export default function Header() {
+export default function Header({ tooltip, link, imageSource }: NavButtonProps) {
   return (
-    <div className="flex flex-col gap-16 items-center">
-      <div className="flex gap-8 justify-center items-center">
-        <a
-          href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <SupabaseLogo />
-        </a>
-        <span className="border-l rotate-45 h-6" />
-        <a href="https://nextjs.org/" target="_blank" rel="noreferrer">
-          <NextLogo />
-        </a>
-      </div>
-      <h1 className="sr-only">Supabase and Next.js Starter Template</h1>
-      <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
-        The fastest way to build apps with{" "}
-        <a
-          href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-          target="_blank"
-          className="font-bold hover:underline"
-          rel="noreferrer"
-        >
-          Supabase
-        </a>{" "}
-        and{" "}
-        <a
-          href="https://nextjs.org/"
-          target="_blank"
-          className="font-bold hover:underline"
-          rel="noreferrer"
-        >
-          Next.js
-        </a>
-      </p>
-      <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
+    <div className="w-full">
+      <nav className="flex h-16 w-full justify-center border-b border-b-foreground/10">
+        <div className="flex w-full max-w-4xl items-center justify-between p-3 text-sm">
+          <NavButton tooltip={tooltip} link={link} imageSource={imageSource} />
+          <AuthButton />
+        </div>
+      </nav>
     </div>
   );
 }
