597b791e5560b73e903a52f9c09c282ec9e9887b
topptobi <tobias.topp@gmx.ch>
2024-07-02 19:24:27 +0200

    TASK-#45: Adding everything to create a typescript doc

--- app/components/Pixel.tsx
+++ app/components/Pixel.tsx
@@ -1,11 +1,25 @@
 import { PixelType } from "@/app/lib/definitions";
 
-export default function Pixel({ pixel }: { pixel: PixelType }) {
+/**
+ *
+ * Pixel component to render a single pixel on the screen.
+ * @param pixel The pixel data to be rendered.
+ *
+ * @returns The JSX element representing the pixel.
+ */
+
+export default function Pixel({ pixel }: { pixel: PixelType }): JSX.Element {
+  /**
+   * Inline style object for positioning and coloring the pixel
+   */
   const style = {
     left: `${pixel.x * 8}px`,
     top: `${pixel.y * 8}px`,
     backgroundColor: pixel.color,
   };
 
+  /**
+   * Return a div element representing the pixel
+   */
   return <div className="absolute h-2 w-2" style={style}></div>;
 }
