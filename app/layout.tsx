f880d8570cc145b15deb6c61d8dbcebc2a54fdf7
lorenz <lorenz.boss@bbzbl-it.ch>
2025-12-07 20:57:13 +0100

    Refactor metadata type and improve cookie handling in createClient

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import { GeistSans } from "geist/font/sans";
+import type { Metadata } from "next";
 import "./globals.css";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : "http://localhost:3000";
 
-export const metadata = {
+export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
   title: "r/Place",
   description: "A school project to recreate r/Place",
